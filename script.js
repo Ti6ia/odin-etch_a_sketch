2fe90722e62c61ead2803aa49f18fdf3509fce6b
Andrea Nichele <a.nichele96@gmail.com>
2022-04-21 00:21:17 +0200

    riesco a colorare

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-/* creo elemento div e lo nomino square*/
-//const square = document.createElement("div");
-/** aggiungo la classe square al div square*/
-//square.className = "square";
-/** stampo per vedere se funziona */
-//console.log(square);
-
-
 /** creo la variabile che conterrà l'html che devo aggiungere per creare i blocchi (square) */
-let squares = '';
-for(let i=0; i<50; i++){
-    squares += '<div class="row">';
-    for(let j=0; j<50; j++){
-        squares += '<div class="square"></div>';
+let htmlString = '';
+/** il ciclo che permette la creazione dell'html per la griglia */
+for(let i=0; i<64; i++){
+    htmlString += '<div class="row">';
+    for(let j=0; j<64; j++){
+        htmlString += '<div class="square"></div>';
     }
-    squares += '</div>';
+    htmlString += '</div>';
 }
-console.log(squares);
 /** inserisco l'html dentro il div "grid" */
-document.querySelector(".grid").innerHTML = squares;
+document.querySelector(".grid").innerHTML = htmlString;
 
+
+/** creo l'array di squares */
+const squares = document.querySelectorAll(".square");
+
+squares.forEach((square) => {
+    square.addEventListener('click', (e) => {
+        e.target.style.background = 'red';
+    })
+});
