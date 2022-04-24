8a43ba30be436101db4956e12d45130e881c4e67
Andrea Nichele <a.nichele96@gmail.com>
2022-04-24 19:10:16 +0200

    ho dato una pulita al codice

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /*CREO LA GRIGLIA*/
 let htmlString = '';
-const gridSize = 64;
+const gridSize = 32;
 
 for(let i=0; i<gridSize; i++){
     htmlString += '<div class="row">';
@@ -69,48 +69,13 @@ squares.forEach((square) => {
 });
 
 
-/** COLORE NORMALE 
-const squares = document.querySelectorAll(".square");
-
-squares.forEach((square) => {
-    square.addEventListener('mouseover', (e) => {
-        if(e.buttons == 1){
-            e.target.classList.add("colored");
-        }  
-    })
-    square.addEventListener('mousedown', (e) => {
-        e.target.classList.add("colored");
-    });
-});
-*/
-
-/** RAINBOW 
-const btnRainbow = document.querySelector(".btnRainbow");
-const r = document.querySelector(':root');
-
-const randColor = () =>  {
-    return "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
-}
-
-function rainbowColorSet(){
-    const rs = getComputedStyle(r);
-    r.style.setProperty('--rainbowColor', randColor());
-    console.log(rs.getPropertyValue('--rainbowColor'))
-}
-
-btnRainbow.addEventListener('click', rainbowColorSet);
-*/
-
-
-
-
 /** TASTO CLEAR */
 const btnClear = document.querySelector(".btnClear");
 
 function clear(){
     squares.forEach((item) => {item.classList.remove("colored");}); 
     squares.forEach((item) => {item.classList.remove("epilector");}); 
-    squares.forEach((item) => {item.style.background = "aqua"}); 
+    squares.forEach((item) => {item.style.background = "#ffffff"}); 
 }
 
 btnClear.addEventListener('click', clear);
