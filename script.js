191a8f1811a37cdb3044987226ff2a8bf2fec66c
Andrea Nichele <a.nichele96@gmail.com>
2022-04-26 12:26:00 +0200

    manca lascelta della grandezza della griglia

--- script.js
+++ script.js
@@ -1,19 +1,53 @@
+/** VARIABILI */
+const r = document.querySelector(':root');
+const rs = getComputedStyle(r);
+
+
 /*CREO LA GRIGLIA*/
 let htmlString = '';
-const gridSize = 32;
+let gridSize = 32;
 
-for(let i=0; i<gridSize; i++){
-    htmlString += '<div class="row">';
-    for(let j=0; j<gridSize; j++){
-        htmlString += '<div class="square"></div>';
+function createGrid(gridSize){
+    htmlString = '';
+    for(let i=0; i<gridSize; i++){
+        htmlString += '<div class="row">';
+        for(let j=0; j<gridSize; j++){
+            htmlString += '<div class="square"></div>';
+        }
+        htmlString += '</div>';
     }
-    htmlString += '</div>';
+    document.querySelector(".grid").innerHTML = htmlString;
 }
 
-document.querySelector(".grid").innerHTML = htmlString;
+createGrid(gridSize);
+
+/*
+const txtGridSize = document.getElementById("gridSize");
+txtGridSize.addEventListener('input', (e) => {
+    gridSize = e.target.value;
+    createGrid(gridSize);
+});
+*/
 
 
-/** COLORARE SQUARES (mk-2)*/
+
+/** colorpicker */
+
+const btnPenColor = document.getElementById("btnPenColor");
+let penColor = '#000000';
+btnPenColor.addEventListener('input', (e) => {
+    penColor = e.target.value;
+    console.log(penColor);
+});
+
+let btnGridColor = document.getElementById("btnGridColor");
+btnGridColor.addEventListener('input', (e) => {
+    r.style.setProperty('--gridColor', e.target.value);
+    console.log(rs.getPropertyValue('--gridColor'));
+});
+
+
+/** COLORARE SQUARES */
     /** vars */
 const squares = document.querySelectorAll(".square");
 const buttons = document.querySelectorAll(".button");
@@ -21,16 +55,14 @@ const btnColor = document.querySelector(".btnColor");
 const btnRainbow = document.querySelector(".btnRainbow");
 const btnEpilector = document.querySelector(".btnEpilector");
 let buttonSelected = "";
-const r = document.querySelector(':root');
 
     /** functions */
 const randColor = () =>  {
     return "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
 }
-function epilectorColorSet(){
-    const rs = getComputedStyle(r);
+function epilectorColorSet(){  
     r.style.setProperty('--epilectorColor', randColor());
-    console.log(rs.getPropertyValue('--epilectorColor'))
+    console.log(rs.getPropertyValue('--epilectorColor'));
 }
 
     /** attiva bottone */
@@ -49,7 +81,7 @@ buttons.forEach((button) => {
 squares.forEach((square) => {
     square.addEventListener('mouseover', (e) => {
         if(e.buttons == 1){
-            if(buttonSelected == "btnColor"){ e.target.classList.add("colored"); }
+            if(buttonSelected == "btnColor"){ e.target.style.background = penColor; }
             if(buttonSelected == "btnRainbow"){ e.target.style.background = randColor(); }
             if(buttonSelected == "btnEpilector"){ 
                 epilectorColorSet();
@@ -59,7 +91,7 @@ squares.forEach((square) => {
         }
     })
     square.addEventListener('mousedown', (e) => {
-        if(buttonSelected == "btnColor"){ e.target.classList.add("colored"); }
+        if(buttonSelected == "btnColor"){ e.target.style.background = penColor;}
         if(buttonSelected == "btnRainbow"){ e.target.style.background = randColor(); }
         if(buttonSelected == "btnEpilector"){ 
             epilectorColorSet();
@@ -75,7 +107,7 @@ const btnClear = document.querySelector(".btnClear");
 function clear(){
     squares.forEach((item) => {item.classList.remove("colored");}); 
     squares.forEach((item) => {item.classList.remove("epilector");}); 
-    squares.forEach((item) => {item.style.background = "#ffffff"}); 
+    squares.forEach((item) => {item.style.background = rs.getPropertyValue('--gridColor')}); 
 }
 
 btnClear.addEventListener('click', clear);
